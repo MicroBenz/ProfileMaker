5afa3fd24f5bb78256a3db9f4e81c2e3dc1d8d18
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2017-04-10 20:37:54 +0700

    Refactor Auth Middleware

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -6,23 +6,34 @@ module.exports = (req, res, next) => {
   console.log(token);
   if (token) {
     token = token.split(' ')[1];
-    jwt.verify(token, process.env.TOKEN_SECRET, (error, decoded) => {
-      if (error) {
-        return res.status(400).send({
-          isTokenRevoked: true,
-          errorMessage: 'Authentication Failed.',
-        });
-      }
-      else {
-        console.log(decoded._doc);
-        req.user = decoded._doc;
-        return next();
-      }
-    });
+    try {
+      const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
+      req.user = decodedToken._doc;
+      return next();
+    }
+    catch (error) {
+      return res.status(400).send({
+        isTokenRevoked: true,
+        errorMessage: 'Authentication Failed.',
+      });
+    }
+    // jwt.verify(token, process.env.TOKEN_SECRET, (error, decoded) => {
+    //   if (error) {
+    //     return res.status(400).send({
+    //       isTokenRevoked: true,
+    //       errorMessage: 'Authentication Failed.',
+    //     });
+    //   }
+    //   else {
+    //     console.log(decoded._doc);
+    //     req.user = decoded._doc;
+    //     return next();
+    //   }
+    // });
   }
-  else {
-    return res.status(403).send({
-      errorMessage: 'No token provided.',
-    });
-  }
+  // else {
+  return res.status(403).send({
+    errorMessage: 'No token provided.',
+  });
+  // }
 };
