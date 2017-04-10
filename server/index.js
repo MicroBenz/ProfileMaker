3107ea12fc4e700cdc39b13aecd68ca1de5d3c5c
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2017-04-10 17:39:57 +0700

    Use dotenv to contain APP_ID and APP_SECRET

--- server/index.js
+++ server/index.js
@@ -2,25 +2,24 @@ const express = require('express');
 const multer = require('multer');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const Strategy = require('passport-facebook').Strategy;
 const FacebookTokenStrategy = require('passport-facebook-token');
+require('dotenv').config();
 
 const api = require('./api');
-const authConfigs = require('./configs/auth');
+
 // Setup Passport authen
 passport.use('facebook-token', new FacebookTokenStrategy({
-  clientID: authConfigs.FACEBOOK_ID,
-  clientSecret: authConfigs.FACEBOOK_SECRET,
-  // callbackURL: authConfigs.FACEBOOK_CALLBACK,
+  clientID: process.env.FACEBOOK_ID,
+  clientSecret: process.env.FACEBOOK_SECRET,
 }, (token, refreshToken, profile, done) => {
   console.log('middleware');
   console.log(profile, token, refreshToken);
-  return done(null, { test: 'hi' });
+  return done(null, profile);
 }));
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(passport.initialize());
 
 app.use('/api/', api);
