6be2a5a921503f11909b2421ebd5289e7f6b1276
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2017-04-20 23:41:20 +0700

    Production is ready

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const FacebookTokenStrategy = require('passport-facebook-token');
+const { resolve } = require('path');
 require('dotenv').config();
 
 mongoose.connect(process.env.DATABASE_HOST);
@@ -22,6 +23,11 @@ app.use(passport.initialize());
 app.use(cors());
 
 app.use('/api/', require('./api'));
+
+app.use('/', express.static(resolve(__dirname, '../dist')));
+app.use('/public', express.static(resolve(__dirname, '../dist/public')));
+app.use('*', (req, res) => res.sendFile(resolve(__dirname, '../dist/index.html')));
+
 
 app.listen(process.env.PORT, () => {
   console.log('ProfileMaker API Listening on 3000');
